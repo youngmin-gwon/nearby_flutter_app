53410e62914c7b3a22a7ba353d45de93eb861a2f
ymgwon-nportverse <ymgwon@nportverse.com>
2023-10-19 11:00:10 +0900

    🎉 Init: 기본 프로젝트 생성

---
+++ android/app/src/main/kotlin/com/nportverse/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nportverse.nft_exchange
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:nft_exchange/app/app.dart';
+import 'package:nft_exchange/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
